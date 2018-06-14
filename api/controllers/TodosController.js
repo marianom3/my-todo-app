2918914c169eead986ba691289c394ef045ddd1b
Mariano Martinez III <mariano.martinez.iii@gmail.com>
2018-06-13 18:48:02 -0700

    added remaining routes to create/update/delete/getall/getsingle todo

--- api/controllers/TodosController.js
+++ api/controllers/TodosController.js
@@ -1,10 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const queries = require('../../queries/TodosQueries');
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
   queries.getAllTodos().then((todos) => {
     res.status(200).json(todos);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  queries.getSingleTodo(parseInt(req.params.id)).then((todo) => {
+    res.status(200).json(todo);
+  });
+});
+
+router.post('/', (req, res) => {
+  queries.createTodo(req.body).then((response) => {
+    if (response.status) {
+      res.status(200).send();
+    }
+    else {
+      res.status(500).send();
+    }
+  });
+});
+
+router.put('/:id', (req, res) => {
+  queries.updateTodo({ id: parseInt(req.params.id), name: req.body.name }).then((response) => {
+    if (response.status) {
+      res.status(200).send();
+    }
+    else {
+      res.status(500).send();
+    }
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  queries.deleteTodo(parseInt(req.params.id)).then((response) => {
+    if (response.status) {
+      res.status(200).send();
+    }
+    else {
+      res.status(500).send();
+    }
   });
 });
 
--- queries/TodosQueries.js
+++ queries/TodosQueries.js
@@ -12,6 +12,59 @@ const getAllTodos = () => {
   });
 };
 
+const getSingleTodo = (id) => {
+  return new Promise((resolve) => {
+    db.one('SELECT * FROM todos WHERE id = $1', id)
+      .then((data) => {
+        return resolve(data);
+      })
+      .catch((err) => {
+        return resolve({});
+      });
+  });
+};
+
+const createTodo = (todo) => {
+  console.log(todo);
+  return new Promise((resolve) => {
+    db.none('INSERT INTO todos(name) VALUES(${name})', todo)
+      .then(() => {
+        return resolve({ status: 'success' });
+      })
+      .catch((err) => {        
+        return resolve({});
+      });
+  });
+};
+
+const updateTodo = (todo) => {  
+  return new Promise((resolve) => {
+    db.none('UPDATE todo SET name = ${name} WHERE id = ${id}', todo)
+      .then(() => {
+        return resolve({ status: 'success' });
+      })
+      .catch((err) => {    
+        return resolve({});
+      });
+  });
+};
+
+const deleteTodo = (id) => {  
+  return new Promise((resolve) => {
+    db.none('DELETE FROM todo WHERE id = ${id}', id)
+      .then(() => {
+        return resolve({ status: 'success' });
+      })
+      .catch((err) => {    
+        return resolve({});
+      });
+  });
+};
+
 module.exports = {
-  getAllTodos: getAllTodos
+  getAllTodos: getAllTodos,
+  getSingleTodo: getSingleTodo,
+  createTodo: createTodo,
+  updateTodo: updateTodo,
+  deleteTodo: deleteTodo
 }
